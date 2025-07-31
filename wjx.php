f3b033b1e334d838a0a0fe555d964a9953dbd8d5
qdpdwjx <105757700+qdpdwjx@users.noreply.github.com>
2025-07-31 20:29:56 +0800

    Update wjx.php

--- wjx.php
+++ wjx.php
@@ -214,8 +214,6 @@ CHC动作电影,http://ucdn.sd.chinamobile.com:8089/00/SNM/CHANNEL00002030/index
 凤凰资讯,https://stream1.freetv.fun/feng-huang-zi-xun-15.m3u8
 凤凰资讯,http://218.104.239.114:9901/tsfile/live/1012_1.m3u8
 凤凰资讯,http://203.205.191.53/qctv.fengshows.cn/live/0701pin72.m3u8
-中天新闻,http://api.mytv666.top/api/hk.php?id=3432626438356234306332353339386439363836663664633634323164393061267469643d74762669643d3939#http://s1.an94tv.cfd/zhongtian/CH-0/AKTV.m3u8
-寰宇新闻,http://push-rtmp-hs-spe-f5.douyincdn.com/live/hyxw01.flv
-TVBS新闻,http://api.mytv666.top/api/hk.php?id=6433326131333730353865386539346239623032306533346562653234623365267469643d74762669643d3938
+中天新闻,https://smart.pendy.dpdns.org/Smart.php?id=ctinews#http://s1.an94tv.cfd/zhongtian/CH-0/AKTV.m3u8
 加密节目_10086,#genre#
 1905电影网,http://cdn.jdshipin.com:8880/1905ts.php?id=1905a
